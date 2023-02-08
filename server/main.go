bad5637ed39240387f4b6e39044fc81ecf8546eb
jesu <jesusmartinezmendez3@gmail.com>
2023-02-08 09:37:29 -0500

    initialize API REST server in application entrypoint

--- server/main.go
+++ server/main.go
@@ -3,10 +3,17 @@ package server
 import (
 	"github.com/jmendezm/drone-dispatch-controller/config"
 	"github.com/jmendezm/drone-dispatch-controller/infra"
-	log "github.com/sirupsen/logrus"
+	restapi "github.com/jmendezm/drone-dispatch-controller/infra/rest_api"
 )
 
 func RunServer(configFilePath string) {
 	conf := config.New(configFilePath)
 	infra.InitLog(conf)
+
+	restAPI := restapi.RestAPI{
+		Configuration: conf,
+		DroneService:  nil,
+	}
+	restAPI.New()
+	restAPI.Start()
 }
